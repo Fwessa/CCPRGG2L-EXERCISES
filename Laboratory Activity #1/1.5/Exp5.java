54e2fb28197c1322e2e006547923263f1a727837
Luisa Dala <111546558+Fwessa@users.noreply.github.com>
2023-01-04 22:31:01 +0800

    Create Exp5.java

---
+++ Laboratory Activity #1/1.5/Exp5.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Exp5 {
+    public static void main(String[] args) throws IOException, FileNotFoundException, ArrayIndexOutOfBoundsException {
+        
+        File inFile = new File("SampleText2.txt");
+        BufferedReader br = new BufferedReader(new FileReader(inFile));
+    
+        String textFile;
+        String data = "";
+
+        try{
+            while((textFile = br.readLine()) != null) {
+            data += textFile;
+        }
+            String[] counter = data.split("[ \t\n,?.!()\"]+");
+            int ln = counter.length -1;
+            System.out.println(" ");
+            
+            
+            //>> To Access the counter Array <<
+            for (int i = 0; i < counter.length; i++) {
+                System.out.print(counter[ln]+ " ");
+                ln--;
+            }
+            
+        }
+        catch(Exception e) {
+            System.out.println("An error has occurred: "+ e.getMessage());
+        }
+        finally{
+            br.close();
+        }
+    }
+}
