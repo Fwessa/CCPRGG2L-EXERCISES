4f0e732cc25e7a0a4d241b51573dea908f44fef7
Luisa Dala <111546558+Fwessa@users.noreply.github.com>
2023-01-11 20:49:21 +0800

    Update Exp5.java

--- Laboratory Activity #1/1.5/Exp5.java
+++ Laboratory Activity #1/1.5/Exp5.java
@@ -1,35 +1,46 @@
 import java.io.*;
+import java.util.*;
 
 public class Exp5 {
     public static void main(String[] args) throws IOException, FileNotFoundException, ArrayIndexOutOfBoundsException {
         
-        File inFile = new File("SampleText2.txt");
-        BufferedReader br = new BufferedReader(new FileReader(inFile));
-    
-        String textFile;
-        String data = "";
+        // >> File Reader <<
+        // Rename input file path if needed
+        
+        File inputFile = new File("dummy.txt");
+        Scanner inFile = new Scanner(inputFile);
+
+        // >> File Writer <<
+        // Rename output file path if needed
+
+        File outFile = new File("reverse.txt");
+        FileWriter fw = new FileWriter(outFile, false);
+        
+        // >> Arraylist <<
+        ArrayList<String> listS = new ArrayList<String>();
 
         try{
-            while((textFile = br.readLine()) != null) {
-            data += textFile;
-        }
-            String[] counter = data.split("[ ()]+");
-            int ln = counter.length -1;
-            System.out.println(" ");
-            
-            
-            //>> To Access the counter Array <<
-            for (int i = 0; i < counter.length; i++) {
-                System.out.print(counter[ln]+ " ");
-                ln--;
+
+            while(inFile.hasNextLine()){
+                listS.add(inFile.nextLine());
             }
-            
-        }
+            Collections.reverse(listS);
+            // >> To Access the listS Array <<
+            for (String data : listS) {
+                String[] counter = data.split("[ \t\n,?.!()\"]+");
+                for (int i = counter.length - 1; i >= 0; i--) {
+                    fw.write(counter[i]+ " ");
+                }
+            fw.write("\n");
+            }  
+       }
         catch(Exception e) {
             System.out.println("An error has occurred: "+ e.getMessage());
         }
         finally{
-            br.close();
+            inFile.close();
+            fw.close();
+            fw.flush();
         }
     }
 }
